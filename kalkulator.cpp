4c11f817be82c0c482501d19ffd025a055f13c2c
wildanindi <wildanindi2@gmail.com>
2024-03-26 18:27:51 +0700

    menambah prosedur pilihan

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -31,3 +31,21 @@ float pembagian(float a, float b)
     return a / b;
 }
 
+int main()
+{
+    int pilihan;
+    do
+    {
+       cout << "Kalkulator sederhana" << endl;
+       cout << "====================" << endl;
+       cout << "1. Penjumlahan" << endl;
+       cout << "2. Pengurangan" << endl;
+       cout << "3. Perkalian" << endl;
+       cout << "4. Pembagian" << endl;
+       cout << "5. keluar" << endl;
+       cout << "====================" << endl;
+       cout << "Masukkan menu pilihan = " << endl;
+       cin >> pilihan;
+
+    }
+}
