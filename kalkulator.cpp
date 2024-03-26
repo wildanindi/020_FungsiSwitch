efe8032cf87721b56299ef43255a68a6319eeb8a
wildanindi <wildanindi2@gmail.com>
2024-03-26 17:51:00 +0700

    menambah prosedur

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -11,4 +11,23 @@ void input()
     cin >> bilB;
 }
 
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int pengurangan(int a, int b)
+{
+    return a - b;
+}
+
+int perkalian(int a, int b)
+{
+    return a * b;
+}
+
+float pembagian(float a, float b)
+{
+    return a / b;
+}
 
