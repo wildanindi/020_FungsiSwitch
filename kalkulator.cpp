03e6f10305da94e0607cf97b7b6e8b26d54ca77d
wildanindi <wildanindi2@gmail.com>
2024-03-26 17:46:39 +0700

    add variable dan parameter

---
+++ kalkulator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+    cout << "Masukkan bilangan Pertama = ";
+    cin >> bilA;
+    cout << "masukkan bilangan kedua = ";
+    cin >> bilB;
+}
+
+
